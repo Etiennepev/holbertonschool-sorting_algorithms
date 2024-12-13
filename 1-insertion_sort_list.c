a3010e58e76ffe4a69470dd859fd16122053790f
DANDEV2433 <alexdani3344@gmail.com>
2024-12-13 10:28:18 +0100

    code added without betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * @insertion_sort_list - sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm
+ * @insertion_sort_list - sort algorithm
+ * description: sorts a doubly linked list of integers with sort algorithm
  * @list: pointer to the node
  */
 
@@ -12,20 +13,25 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current = (*list)->next;
 
 	if (list == NULL)
-                return;
+		return;
 	while (current)
 	{
-	
+
 	listint_t *next_node = current->next;
 	listint_t *prev_node = current->prev;
 
 	while (prev_node && current->n < prev_node->n)
 	{
+	/**
+	* Détache le nœud courant de sa position actuelle
+	*/
 	if (current->next)
 	current->next->prev = current->prev;
 	if (current->prev)
 	current->prev->next = current->next;
-	
+	/**
+	* réinsère le noeud courant avant le noeud précédent
+	*/
 	current->prev = prev_node->prev;
 	current->next = prev_node;
 	if (prev_node->prev)
@@ -33,7 +39,18 @@ void insertion_sort_list(listint_t **list)
 	else
 	*list = current;
 	prev_node->prev = current;
+	/**
+	* imprime la liste après chaque échange
+	*/
 	print_list(*list);
+	/**
+	* met à jour prev_node pour continuer à vérifier les noeuds précédents
+	*/
 	prev_node = current->prev;
 	}
+	/**
+	 * passe au noeud suivant
+	*/
 	current = next_node;
+	}
+}
